7d00235b27ef3e326d0078e1429ea69bea6f96fa
Nathan Brown <nathanbrown@Nathans-MacBook-Pro.local>
2021-08-30 21:01:14 -0600

    feat: Add looping feature/input validation for cases to runner file

--- mastermind.rb
+++ mastermind.rb
@@ -1,11 +1,10 @@
 #require will go here
 
 puts "Welcome to MASTERMIND!\n\nWould you like to (p)lay, read the (i)nstructions, or (q)uit?\n>"
-response = gets.chomp
-# until response == "p" || response == "play" ||
-#       response == "i" || response == "instructions" ||
-#       response == "q" || response == "quit"
-loop do
+response = gets.chomp.downcase
+until response == "p" || response == "play" ||
+      response == "i" || response == "instructions" ||
+      response == "q" || response == "quit"
 if response == "p" || response == "play"
   # game flow
 elsif response == "i" || response == "instructions"
@@ -14,6 +13,6 @@ elsif response == "q" || response == "quit"
   break
 else
   puts "You entered #{response}. Would you like to (p)lay, read the (i)nstructions, or (q)uit?\n>"
-  next
+  response = gets.chomp.downcase
 end
 end
