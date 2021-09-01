7955fadb6c16335cd3422e8900f1a59436804f64
Nathan Brown <nathanbrown@Nathans-MacBook-Pro.local>
2021-09-01 11:07:26 -0600

    Revert runner file to previous format

--- mastermind.rb
+++ mastermind.rb
@@ -2,20 +2,20 @@
 
 puts "Welcome to MASTERMIND!\n\nWould you like to (p)lay, read the (i)nstructions, or (q)uit?\n>"
 response = gets.chomp.downcase
-valid_responses = ["p",
-             "play",
-             "i",
-             "instructions",
-             "q",
-             "quit"]
+# responses = ["p", "play", "i", "instructions", "q", "quit"] -- leave for refactoring
+# until responses.include?(response)
+until response == "p" || response == "play" ||
+      response == "i" || response == "instructions" ||
+      response == "q" || response == "quit"
 
-until valid_responses.include?(response)
 if response == "p" || response == "play"
   # game flow
 elsif response == "i" || response == "instructions"
   puts "The object of MASTERMIND is to guess a secret code consisting of a series of 4
-colored pegs. Each guess results in feedback narrowing down the possibilities of the
-code."
+  colored pegs. Each guess results in feedback narrowing down the possibilities of the
+  code."
+  puts "You entered #{response}. Would you like to (p)lay, read the (i)nstructions, or (q)uit?\n>"
+  response = gets.chomp.downcase
 elsif response == "q" || response == "quit"
   break
 else
