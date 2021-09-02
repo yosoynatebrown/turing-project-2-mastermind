2fd979dcf4d97850eaeab3d0e2d6f1775756bf76
Craig Cardwell <craigcardwell@Craigs-MacBook-Pro.local>
2021-09-02 12:36:36 -0600

    feat: add compare feature for player guesses

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,18 +7,22 @@ class Turn
     colors_correct = 0
     colors_in_correct_positions = 0
 
-    guess = 'rrgb'
-    array = guess.split ''
-    p array
+    hidden_code = ['g', 'r', 'r', 'b']
+    guess = 'rrgb' # This will probably wind up being equal to guess.gets from the game class
+    individual_guesses = guess.split ''
+
+    individual_guesses.map.with_index {|color, i| color == hidden_code[i]}
 
 
-
+    colors_correct += (individual_guesses & hidden_code.flatten).count
+    colors_in_correct_positions += individual_guesses.map.with_index {|color, i| color == hidden_code[i]}.count(true)
+  
     #guess argument is equal to 'rrgb'
     #which means the 'b' and the second 'r' in guess are in
     #the correct positions relative to hidden_code below
     #additionally all 3 colors used ('r', 'g', 'b') are correct
 
-    hidden_code = ['g', 'r', 'r', 'b']
+    # hidden_code = ['g', 'r', 'r', 'b']
 
     # your task, should you choose to accept it, is to figure
     # out how to calculate colors_correct and
