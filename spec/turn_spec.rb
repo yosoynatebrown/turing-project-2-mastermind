eaeb5f97238424f6a6490925aa82fccfc9124580
Craig Cardwell <craigcardwell@Craigs-MacBook-Pro.local>
2021-09-02 21:10:43 -0600

    test: add spec for compare and identifying winning guess

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,21 +3,18 @@ require 'rspec'
 
 describe Turn do
   describe "#initialize" do
-    xit 'exists' do
-      turn = Turn.new
+    it 'exists' do
+      turn = Turn.new('rrgb')
     expect(turn).to be_a(Turn)
     end
   end
   describe "#compare" do
     it 'compares guess string to hidden code and returns num correct and num in correct place' do
-      turn = Turn.new
+      turn = Turn.new('rrgb')
 
-
-
-
-    expect(turn.compare('rrgb')).to eq([3, 2])
-    expect(turn.compare('grrb')).to eq([3, 4])
-    expect(turn.compare('brgy')).to eq([3, 1])
+    expect(turn.compare).to eq([3, 2])
+    # expect(turn.compare('grrb')).to eq([3, 4])
+    # expect(turn.compare('brgy')).to eq([3, 1])
     end
   end
 end
