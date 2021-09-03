b5561f525138231fff1287d26022e2a0f9e6cbde
Nathan Brown <nathanbrown@Nathans-MacBook-Pro.local>
2021-09-02 19:44:58 -0600

    feat: Add sequence and code generator classes

--- spec/code_generator_spec.rb
+++ spec/code_generator_spec.rb
@@ -1,7 +1,19 @@
 require 'code_generator'
 require 'rspec'
+require 'sequence'
 
 
 describe CodeGenerator do
-  xit
+   it 'exists' do
+     code_generator = CodeGenerator.new
+
+
+    expect(code_generator).to be_a(CodeGenerator)
+   end
+
+   it '#all_possible_codes' do
+     code_generator = CodeGenerator.new
+
+    expect(code_generator.hidden_code).to be_an(Array)
+   end
 end
