af0eba62eb7c48f0d4d44d861770dc95abbc2486
Craig Cardwell <craigcardwell@Craigs-MacBook-Pro.local>
2021-09-06 15:53:43 -0600

    test: edit code generator test to confirm unique codes generated

--- spec/code_generator_spec.rb
+++ spec/code_generator_spec.rb
@@ -1,6 +1,6 @@
 require 'code_generator'
 require 'rspec'
-require 'sequence'
+
 
 
 describe CodeGenerator do
@@ -17,9 +17,16 @@ describe CodeGenerator do
     expect(code_generator.all_possible_codes).to be_an(Array)
    end
 
-   it 'can create a sample hidden code'do
+   it 'can create a sample hidden code' do
      code_generator = CodeGenerator.new
 
      expect(code_generator.hidden_code).to be_an(Array)
+   end
+
+   it 'can create multiple unique hidden codes' do
+     code_generator1 = CodeGenerator.new
+     code_generator2 = CodeGenerator.new
+
+     expect(code_generator1.hidden_code).not_to eq(code_generator2.hidden_code)
    end
 end
