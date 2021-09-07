1485b0e19fabaf00ea7baaaf48972244f4dd8199
Nathan Brown <nathanbrown@Nathans-MacBook-Pro.local>
2021-09-06 21:44:04 -0600

    Add history extension to player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,13 @@
 class Player
   attr_accessor :quit,
                 :won,
-                :number_of_guesses
+                :number_of_guesses,
+                :history
   def initialize
     @number_of_guesses = 0
     @won = false
     @quit = false
+    @history = []
   end
 
   def increment_number_of_guesses
@@ -15,5 +17,8 @@ class Player
   def win
     @won = true
   end
-  
+
+  def add_to_history(guess)
+    @history << guess.upcase
+  end
 end
